5323d7b5cad64bf2bb0d271f221502fad11d43f9
Sebastiaan Provost <sepr@opsec.be>
2019-11-13 02:41:22 +0000

    Add command schedules for server and scan status

--- app/Console/Commands/CheckScanStatus.php
+++ app/Console/Commands/CheckScanStatus.php
@@ -15,7 +15,7 @@ class CheckScanStatus extends Command
      *
      * @var string
      */
-    protected $signature = 'checkscanstatus {id}';
+    protected $signature = 'checkscanstatus';
 
     /**
      * The console command description.
@@ -49,28 +49,31 @@ class CheckScanStatus extends Command
     {
         //
         $scan_status = true;
-        $scan = $this->scans->findWhere([['id', '=', $this->argument('id')], ['scan_status', '=', 1]]);
-        if ($scan->count() == 0) {
-            $this->error('No scan with that ID.');
+        $scans = $this->scans->findWhere(['scan_status', '=', 1]);
+        if ($scans->count() == 0) {
+            $this->error('No scans finished.');
         } else {
-            $scan_servers = $this->servers->findWhere(['scan_id', '=', $this->argument('id')]);
-            foreach ($scan_servers as $server) {
-                $this->ssh = new SSHHelper($server->uuid, $server->user, $server->ip, $server->port, $server->public_key, $server->private_key);
-                $scan_status = Str::contains($this->ssh->read_file('/' . $server->user . '/'.$scan[0]->id . '_' . str_replace(' ', '_', $scan[0]->name).'.json'), 'finished: 1');
-                //
-                // Masscan version supported: 1.0.4, we really on the finish tag. for version 5, we'll have to check the process list to see if it finished. Or add extra file
-                //
-                if($scan_status == false){
-                    break;
+            foreach($scans as $scan)
+            {
+                $scan_servers = $this->servers->findWhere(['scan_id', '=', $scan->id]);
+                foreach ($scan_servers as $server) {
+                    $this->ssh = new SSHHelper($server->uuid, $server->user, $server->ip, $server->port, $server->public_key, $server->private_key);
+                    $scan_status = Str::contains($this->ssh->read_file('/' . $server->user . '/'.$scan->id . '_' . str_replace(' ', '_', $scan->name).'.json'), 'finished: 1');
+                    //
+                    // Masscan version supported: 1.0.4, we really on the finish tag. for version 5, we'll have to check the process list to see if it finished. Or add extra file
+                    //
+                    if($scan_status == false){
+                        break;
+                    }
                 }
-            }
 
-            if ($scan_status) {
-                $this->scans->update($this->argument('id'), ['scan_status' => 2]);
-/*                foreach ($scan_servers as $server) {
-                    $this->servers->update($server->id, ['scan_id' => 0]);
-                }*/
-                $this->info('Scan has finished.');
+                if ($scan_status) {
+                    $this->scans->update($scan->id, ['scan_status' => 2]);
+                    /*                foreach ($scan_servers as $server) {
+                                        $this->servers->update($server->id, ['scan_id' => 0]);
+                                    }*/
+                    $this->info('Scan ' . $scan->id . ' has finished.');
+                }
             }
         }
     }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('checkserverstatus')->everyFiveMinutes();
+        $schedule->command('checkscanstatus')->everyFiveMinutes();
     }
 
     /**
